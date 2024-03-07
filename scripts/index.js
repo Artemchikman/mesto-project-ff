fd2ae57a8c8b3d80092e8ab8f38bfc7ee91d9f81
Artem <artem-10.98@yandex.ru>
2024-03-07 17:43:47 +0300

    Второе отправление

--- scripts/index.js
+++ scripts/index.js
@@ -8,25 +8,44 @@
 
 // @todo: Вывести карточки на страницу
 
-const placesList = document.querySelector('.places__list'); /*Вводим переменную и получаем доступ к классу places__list*/
-const templateElement = document.querySelector('#card-template').content.querySelector('.card'); /*Вводим переменную и получаем доступ к шаблону карточек*/
+/*Возвращаем элемент с классом 'places__list' */
+const placesList = document.querySelector(".places__list");
+
+/* Получаем элемент шаблона с идентификатором 'card-template', а затем первый дочерний элемент .card*/
+const templateElement = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
+
+/* Создаем новый элемент карточки путем клонирования элемента шаблона и возвращаем его*/
 function createCard(card, deleteCard) {
-  const cardElement = templateElement.cloneNode(true); /*получаем Клон карточки template */
-  const cardTitle = cardElement.querySelector('.card__title');
+  /* Клонирует элемент шаблона*/
+  const cardElement = templateElement.cloneNode(true);
+
+  /*Устанавливаем текстовое содержимое заголовка карточки на название карточки*/
+  const cardTitle = cardElement.querySelector(".card__title");
   cardTitle.textContent = card.name;
-  const cardImage = cardElement.querySelector('.card__image');
-  cardImage.setAttribute('src', card.link);
-  cardImage.setAttribute('alt', card.name);
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', deleteCard);
-return cardElement;
+
+  /* Устанавливаем атрибуты source и alt изображения карты для ссылки на карту и ее названия */
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.setAttribute("src", card.link);
+  cardImage.setAttribute("alt", card.name);
+
+  /*Получаем кнопку удаления и добавляем к ней прослушиватель событий, который вызывает функцию deleteCard при нажатии*/
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  /*Возвращаем элемент card*/
+  return cardElement;
 }
 
-function deleteElement(event) {  /*Функция для удаления карточек*/
-event.target.closest('.card').remove();
+/* Удаляем ближайший элемент .card при вызове*/
+function deleteElement(event) {
+  event.target.closest(".card").remove();
 }
 
-initialCards.forEach(function(card){ /* Цикл который добавляет карточки */
-  card = createCard(card, deleteElement);
-  placesList.appendChild(card);
-}) 
+/* Перебираем каждую карточку в исходных карточках и добавляем ее в placesList*/
+initialCards.forEach(function (card) {
+  /*Создаем новую карточку и добавляем ее в  placesList*/
+  const newCard = createCard(card, deleteElement);
+  placesList.appendChild(newCard);
+});
